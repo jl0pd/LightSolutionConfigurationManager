0dd89bd5c8a2e771951b853a61620464cafc8b09
Denis Proshutinskii <jl0pd@outlook.com>
2021-04-08 09:56:21 +0700

    Update to net5

--- src/LightSolutionConfigurationManager/Program.fs
+++ src/LightSolutionConfigurationManager/Program.fs
@@ -3,10 +3,11 @@
 open Elmish
 open Avalonia
 open Avalonia.Controls.ApplicationLifetimes
-open Avalonia.Input
 open Avalonia.FuncUI
 open Avalonia.FuncUI.Elmish
 open Avalonia.FuncUI.Components.Hosts
+open Avalonia.Themes.Fluent
+open Avalonia.Input
 
 type MainWindow() as this =
     inherit HostWindow()
@@ -14,22 +15,30 @@ type MainWindow() as this =
         base.Title <- "LightSolutionConfigurationManager"
         base.Width <- 400.0
         base.Height <- 400.0
-        
+
         //this.VisualRoot.VisualRoot.Renderer.DrawFps <- true
         //this.VisualRoot.VisualRoot.Renderer.DrawDirtyRects <- true
 
+#if DEBUG
+        this.AttachDevTools(KeyGesture(Key.F12))
+#endif
 
-        Elmish.Program.mkSimple (fun () -> Counter.init) Counter.update Counter.view
+
+        Program.mkSimple (fun () -> Counter.init) Counter.update Counter.view
         |> Program.withHost this
+#if DEBUG
+        |> Program.withConsoleTrace
+#endif
         |> Program.run
 
-        
+
 type App() =
     inherit Application()
 
     override this.Initialize() =
         this.Styles.Load "avares://Avalonia.Themes.Default/DefaultTheme.xaml"
         this.Styles.Load "avares://Avalonia.Themes.Default/Accents/BaseDark.xaml"
+        this.Styles.Add (FluentTheme(baseUri= null, Mode = FluentThemeMode.Dark))
 
     override this.OnFrameworkInitializationCompleted() =
         match this.ApplicationLifetime with
